8c0903b4bcd9d2f4d5340a27db512fc2dc02444c
Matteo Castagna <matteo.castagna@gmail.com>
2021-01-26 18:28:58 +0000

    Update

--- basic.R
+++ basic.R
@@ -1,19 +1,30 @@
 library(ggplot2)
-library(dplyr)
 
+# WIPE ALL OUT ####
 rm(list = ls())
 
+##### THIS IS THE FUNCTION OUTPUT #####################
 f <- function(x) {
-  y = x^3
+
+  y = x^2
+  
 }
+#######################################################
 
-x = seq(from = -2, to = 2, by = 0.01)
+# FUNCTION DOMAIN (A SELECTION OF) ####
+x = seq(from = -2, to = 2, by = 0.01) 
 
+# DATASET WITH DOMAIN/RANGE (INPUT/OUTPUT) ####
 data <- data.frame(x = x,
-                   y = f(x))
+                   y = f(x))          
 
+# CREATE CHART #####
 chart <- ggplot(data, aes(x, y)) +
-  geom_line() +
-  theme_bw()
+  geom_line(color = "red") +
+  geom_hline(yintercept = 0) +
+  geom_vline(xintercept = 0) +
+  theme_bw() + 
+  labs(title = paste0(gsub("\\{|\\}", "", body(f)), collapse = " "))
 
+# SHOW ME THE CHART ####
 print(chart)
